3710708a30dbcf57e2d8d0178d309b5e88ff638f
gcrowell <graham.crowell@gmail.com>
2018-03-26 01:03:47 -0700

    #15 ExchangeRates Example https://www.apollographql.com/docs/react/essentials/get-started.html

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
+import PropTypes from 'prop-types';
+
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
+
 import JobPage from './Components/JobPage/JobPage'
 import ShiftPage from './Components/ShiftPage/ShiftPage'
 import CompletionPage from './Components/CompletionPage/CompletionPage'
+import ExchangeRates from './Components/ExchangeRates'
 
+const client = new ApolloClient({
+  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static defaultProps = {
+    client
+  }
+
   render() {
+
+    const { client } = this.props;
+
     return (
-      <MuiThemeProvider>
-        <Router>
-          <div>
-            <Route exact={true} path="/" component={JobPage} />
-            <Route exact={true} path="/Jobs" component={JobPage} />
-            <Route exact={true} path="/Shift" component={ShiftPage} />
-            <Route exact={true} path="/Complete" component={CompletionPage} />
-          </div>
-        </Router>
-      </MuiThemeProvider>
+      <ApolloProvider client={client}>
+        <MuiThemeProvider>
+          <Router>
+            <div>
+              <Route exact={true} path="/" component={ExchangeRates}/>
+              <Route exact={true} path="/Jobs" component={JobPage}/>
+              <Route exact={true} path="/Shift" component={ShiftPage}/>
+              <Route exact={true} path="/Complete" component={CompletionPage}/>
+            </div>
+          </Router>
+        </MuiThemeProvider>
+      </ApolloProvider>
     );
   }
 }
+
+
+App.propTypes = {
+  client: PropTypes.object.isRequired,
+};
 
 export default App;
